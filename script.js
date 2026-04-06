4af3a78c1ab5ebd23d266660ef6a5a9ec9a2db55
Joao Pedro <jpsb2@aluno.ifnm.edu.br>
2026-04-06 09:41:39 -0300

    kjh comitit

---
+++ script.js
@@ -0,0 +1,14 @@
+<
+        /*
+        window.alert("Hello world")
+        window.confirm("Estou aprendendo JS")
+        window.prompt("Qual o seu nome?")
+        */
+       function entrar(){
+        let mudarCor = document.querySelector('.quadrado')
+        mudarCor.style.backgroundColor = 'blue'
+       }
+       function sair(){
+        let mudarCor = document.querySelector('.quadrado')
+        mudarCor.style.backgroundColor = 'red'
+       }
